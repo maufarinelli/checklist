37b15632b72638fad779ca0c1edf066b0f222573
Farinelli, Mauricio <Mauricio.Farinelli@Kronos.com>
2017-10-11 16:36:09 -0400

    Adding some styles to Checkbox

--- src/components/common/checkbox/CheckboxInput.js
+++ src/components/common/checkbox/CheckboxInput.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './checkbox.css';
 
 const CheckboxInput = ({id, name, value, label}) => (
-    <div className="form-check">
+    <div className="form-check checkbox-input">
         <label className="form-check-label">
           <input id={id} className="form-check-input" type="checkbox" name={name} value={value} />
           {label}
